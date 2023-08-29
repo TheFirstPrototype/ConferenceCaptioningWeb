69553377dbe1ca8e40ba99c1f8475a69165fe7f4
Saamer Mansoor <i@saamer.me>
2023-08-28 21:08:34 -0400

    Updated stream name to WeTech

--- wetech/script.js
+++ wetech/script.js
@@ -101,7 +101,7 @@ function stopTimer() {
 }
 
 function getTranscript() {
-  var url="https://script.google.com/macros/s/AKfycbzqOWlC9bT6TtLp1QJLzAkwDZJKTcCZYnoDhN4JIMXTo5lEvtPruYb-3vrILj__yO_A/exec?streamName=ExpressTop";
+  var url="https://script.google.com/macros/s/AKfycbzqOWlC9bT6TtLp1QJLzAkwDZJKTcCZYnoDhN4JIMXTo5lEvtPruYb-3vrILj__yO_A/exec?streamName=WeTech";
   // To avoid using JQuery, you can use this https://stackoverflow.com/questions/3229823/how-can-i-pass-request-headers-with-jquerys-getjson-method
   $.getJSON(
     url,

ec5e2d8db423baf8c83d80f8a4e1e69a8fd02ab0
Saamer Mansoor <i@saamer.me>
2025-02-04 11:27:00 -0500

    Added new videos in all languages

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ This repository contains the code of the Landing Page website for the [BeAware -
 The app itself is open-source and is written in SwiftUI. Here's the [code for the app](https://github.com/philparkus/BeAware)
 
 To briefly understand what the app does, you can watch [this video](https://vimeo.com/950840441)
+
+**Automating Translations**
+--------
+- Try to set all the languages to be in english in all the files so that we can do a search and replace
+- Use HTML Translator sheet from here to create translations: https://docs.google.com/spreadsheets/d/1ocn1WHA4--EabfJ2eEZwZjz4orCIKo18rawDiJCZVYs/edit?gid=1537366283#gid=1537366283
+- Then get the JSON value from here https://codepen.io/saamerm/pen/NWVZyEm and store it in a new file like languages-demoVideos.json
+- Create a copy of demoVideo.sh and make the changes needed (especially the json file name). There's two parts to the shell script.
+- Only commit needed files
---
+++ demoVideos.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# PART 1: Directory to search for index.html files
+# DIR="."
+
+# # Loop through all index.html files in the directory and its subdirectories
+# find "$DIR" -name "*.html" | while read -r file; do
+#   # Extract the content of line 6
+#   value=$"                                        Preview Video"
+  
+#   # Replace line 729 with the content of the value variable
+#   sed -i '' "729s|.*|$value|" "$file"
+  
+#   echo "Updated line 729 in $file"
+# done
+
+# PART 2: Directory to search for index.html files
+#!/bin/bash
+
+# JSON file containing translations
+TRANSLATION_FILE="languages-demoVideos.json"
+# Define the language codes
+languageCodes=("ES" "FR" "PT" "AR" "RU" "DE" "UK" "HI" "UR" "YO" "ID" "IT" "JA" "SW" "PL" "VI" "RO" "zh-Hant" "ZH" "HR" "FA" "NL" "KO" "SV" "HU" "SQ")
+
+# Check if translation file exists
+if [ ! -f "$TRANSLATION_FILE" ]; then
+    echo "Translation file '$TRANSLATION_FILE' not found."
+    exit 1
+fi
+
+# Create a temporary file to store JSON data
+tmp_file=$(mktemp)
+
+# Extract translations to temporary file
+jq -c ".[]" "$TRANSLATION_FILE" > "$tmp_file"
+
+# Function to escape special characters for sed
+escape_sed() {
+    echo "$1" | gsed -e 's/[\/&]/\\&/g' -e 's/(/\\(/g' -e 's/)/\\)/g'
+}
+
+# Iterate through each language and perform translations
+for lang in "${languageCodes[@]}"; do
+    lowerCode=$(echo "$lang" | tr '[:upper:]' '[:lower:]')
+    html_file=index-${lowerCode}.html
+    if [ ! -f "$html_file" ]; then
+        echo "HTML file '$html_file' for language '$lang' not found."
+    else
+        echo "Applying translations for language '$lang' to HTML file '$html_file'..."
+
+        # Generate sed commands for current language
+        sed_commands=""
+        while IFS= read -r line; do
+            en_value=$(echo "$line" | jq -r '.EN // empty')
+            # echo $en_value #Uncomment to debug
+            lang_value=$(echo "$line" | jq -r ".$lang // empty")
+            # echo $lang_value #Uncomment to debug
+
+            if [ -n "$en_value" ] && [ -n "$lang_value" ]; then
+                escaped_en_value=$(escape_sed "$en_value")
+                escaped_lang_value=$(escape_sed "$lang_value")
+                # echo $escaped_en_value #Uncomment to debug
+                # echo $escaped_lang_value #Uncomment to debug
+                # Use different delimiter in sed command to avoid issues with special characters
+                sed_commands+="s#${escaped_en_value}#${escaped_lang_value}#g;"
+            fi
+        done < "$tmp_file"
+
+        # Perform sed replacement on the HTML file
+        gsed -i.bak -e "$sed_commands" "$html_file"
+        echo "Translations applied for language '$lang'."
+    fi
+done
+
+echo "All translations applied successfully."
